7c1715984f1489ea98db8406bbe8ed564dc2f62a
wellhenri <welissonh80@gmail.com>
2022-02-07 15:47:25 -0300

    added numeros animados

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 import initScrollSuave from "./modules/scroll-suave.js";
-initScrollSuave(); // Scroll suave da página
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-initAnimacaoScroll(); // Scroll suave da página
 import initAccordion from "./modules/accordion.js";
-initAccordion(); //Accordion da pagina
 import initTabNav from "./modules/nav-tab.js";
-initTabNav(); // navegação animada no meu nav
 import initModal from "./modules/modal.js";
-initModal(); // abre modal de login quando ativado
 import initTooltip from "./modules/tooltip.js";
-initTooltip(); // abre o toltip onde possivel
 import initDropdownMenu from "./modules/dropdown-menu.js";
-initDropdownMenu(); // dropdown do menu
 import initMenuMobile from "./modules/menu-mobile.js";
-initMenuMobile(); //Menu Mobile
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
