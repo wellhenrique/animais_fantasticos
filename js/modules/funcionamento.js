1429bc5ecdd6f0102e9acd29438d4dce8e284282
wellhenri <welissonh80@gmail.com>
2022-02-07 17:12:00 -0300

    added horario de funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,2 +1,16 @@
-export default function initFuncionamento() {}
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horariosSemana = funcionamento.dataset.horario.split(",").map(Number);
 
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horaAgora = dataAgora.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) != -1 ? true : false;
+  const horarioAberto =
+    horaAgora >= horariosSemana[0] && horaAgora < horariosSemana[1];
+
+  if (semanaAberto && horarioAberto) funcionamento.classList.add("aberto");
+  else funcionamento.classList.remove("aberto");
+}
