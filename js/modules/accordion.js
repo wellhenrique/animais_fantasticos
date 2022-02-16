6a04435ad27033a8058182d0f80fbca32b3da773
wellhenri <welissonh80@gmail.com>
2022-02-16 20:18:54 -0300

    accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,21 +1,29 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt',
-  );
-  const activeClass = "ativo";
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(links) {
+    this.activeClass = "ativo";
+    this.accordionList = document.querySelectorAll(links);
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  // Add eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.toggleAccordion(item);
+      });
     });
   }
-  console.log("teste");
+
+  // inicia funcao
+  init() {
+    if (this.accordionList.length) {
+      // ativa o primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+  }
 }
